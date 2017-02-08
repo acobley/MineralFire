e100cf34c6ec3d293394851aec23acac16a9e24b
Andy <aecobley@dundee.ac.uk>
2017-02-08 18:47:36 +0000

    getting there

--- Volca_Pulse.ino
+++ Volca_Pulse.ino
@@ -1,14 +1,15 @@
 
-int Counter=0;
-boolean state[]={false,false,false,false};
-int pins[]={13,12,11,10};
-int pulsecount[]={0,0,0,0};
+int Counter = 0;
+boolean state[] = {false, false, false, false};
+int pins[] = {13, 12, 11, 10};
+int pulsecount[] = {0, 0, 0, 0};
 
 
-int SeqLength=8;
-int Bars=1;
+int SeqLength = 8;
+int Bars = 1;
 
 const byte interruptPin = 2;
+
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -18,49 +19,38 @@ void setup() {
   pinMode(11, OUTPUT);
   pinMode(10, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
-   attachInterrupt(digitalPinToInterrupt(interruptPin), StartCount, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), StartCount, RISING);
 }
 
-void SetPin(int pin, boolean state){
-    digitalWrite(pins[pin], state);
+
+
+void SetPin(int pin, boolean state) {
+  digitalWrite(pins[pin], state);
 }
 
-void StartCount(){
+void StartCount() {
   Counter++;
- 
-  if (Counter >=SeqLength){
-       StartCount(1);
-       Counter=0;
+  if (Counter >= SeqLength * Bars) {
+    Pulse();
+    Counter = 0;
   }
-    
 }
 
-void StartCount(int pin){
-  pulsecount[pin]=0;
-}
 
-void Pulse(){
-  for (int i=0;i<4;i++){
-    if (pulsecount[i]<16){
-      if (i==0){
-          digitalWrite(LED_BUILTIN, HIGH);
-      }
-       SetPin(i,HIGH);
-    }   
-    else {
-      if (i==0){
-           digitalWrite(LED_BUILTIN, LOW); 
-      }
-        SetPin(i,LOW);
-    }    
-    pulsecount[i]++;
+void Pulse() {
+  int i = 0;
+  digitalWrite(LED_BUILTIN, HIGH);
+  SetPin(0, HIGH);
+  for (i = 0; i < 16; i++) {
+    delay(1);
   }
+  digitalWrite(LED_BUILTIN, LOW);
+  SetPin(0, LOW);
+
 }
 // the loop function runs over and over again forever
 void loop() {
- 
-  delay(1);
-  Pulse();
-  
-  
+
+
+
 }
