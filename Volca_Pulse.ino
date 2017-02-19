a08de827fbf47eee7e7fa14cfce3263952136ac2
Andy <aecobley@dundee.ac.uk>
2017-02-19 19:46:56 +0000

    LED support

--- Volca_Pulse.ino
+++ Volca_Pulse.ino
@@ -1,9 +1,10 @@
+#include <SoftwareSerial.h>
 
 int Counter = 0;
 boolean state[] = {false, false, false, false};
 int pins[] = {13, 12, 11, 10};
 int BarsCount[]={3,1,2,2};
-int SeqLengths[]={8,4,8,4};
+int SeqLengths[]={16,8,16,8};
 int BarCount =0; //the element of the BarsCount Array
 int CurrentBar=1; //how far through the current Bars we are
 
@@ -11,7 +12,8 @@ int SeqLength = SeqLengths[BarCount];
 int Bars = 1;
 
 const byte interruptPin = 2;
-
+SoftwareSerial Serial7Segment(7, 8); //RX pin, TX pin
+char tempString[10]; //Used for sprintf
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -23,6 +25,8 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   attachInterrupt(digitalPinToInterrupt(interruptPin), StartCount, RISING);
   Serial.begin(9600);  
+  Serial7Segment.begin(9600); //Talk to the Serial7Segment at 9600 bps
+  Serial7Segment.write('v'); //Reset the display - this forces the cursor to return to the beginning of the display
   Bars=BarsCount[BarCount];
 }
 
@@ -35,8 +39,11 @@ void SetPin(int pin, boolean state) {
 
 void StartCount() {
   Counter++;
-  Serial.print(Counter); 
-  Serial.print("  :  ");
+  int tmp=(SeqLength * Bars)*100+Counter;
+  
+  sprintf(tempString, "%4d", tmp); //Convert deciSecond into a string that is right adjusted
+  Serial7Segment.print(tempString); //Send serial string out the soft serial port to the S7S
+
   if (Counter >= (SeqLength * Bars)) {
     Pulse();
     Counter = 0;
@@ -56,13 +63,13 @@ void StartCount() {
 
 void Pulse() {
   
-  Serial.println(" Pulse");
+  
   int i=0;
   SetPin(0, HIGH);
   for (i=0;i<1000;i++){
     int j=i*10;
   }
-Serial.println(" end");
+
   SetPin(0, LOW);
 
 }
@@ -70,7 +77,8 @@ Serial.println(" end");
 void loop() {
 
 while (true){
-  delay(1);
+    int cycles= BarCount;
+
 }
 
 }
