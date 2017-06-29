8ac5be9145cccaa9ba708a768e8d53c9b440859b
Andy <aecobley@dundee.ac.uk>
2017-06-29 19:31:39 +0100

    array count works

--- Volca_Pulse.ino
+++ Volca_Pulse.ino
@@ -1,20 +1,22 @@
 #include <SoftwareSerial.h>
 
 volatile int DivPulseCounter = 0;
+volatile int NumPulseCounter = 0;
 boolean state[] = {false, false, false, false};
-volatile boolean running =false;
+volatile boolean running = false;
 int pins[] = {13, 12, 11, 10};
-int DivFactors[]={1,2,1,1};
-int NumberBeats[]={8,8,4,4};
-volatile int CurrentFactor =0; //the element of the DivFactors Array
-volatile int CurrentBeatArray=0; //how far through the current Bars we are
+int DivFactors[] = {4, 2, 1, 1};
+int NumberPulses[] = {8, 8, 4, 4};
 
-int CurrentDivFactor = DivFactors[CurrentFactor];
-int CurrentBarsLength = NumberBeats[CurrentBeatArray];;
+volatile int CurrentPtr = 0; //how far through the current Bars we are
+
+int CurrentDivFactor = DivFactors[CurrentPtr];
+int CurrentPulseLength = NumberPulses[CurrentPtr];
+
 
 const byte interruptPin = 2;
 const byte switchinterruptPin = 3;
-const int Switch=9; // We need this connection because we can't read the interrupt pin (I think!)
+const int Switch = 9; // We need this connection because we can't read the interrupt pin (I think!)
 SoftwareSerial Serial7Segment(7, 8); //RX pin, TX pin
 char tempString[10]; //Used for sprintf
 
@@ -25,66 +27,80 @@ void setup() {
   pinMode(12, OUTPUT);
   pinMode(11, OUTPUT);
   pinMode(10, OUTPUT);
-  pinMode(Switch,INPUT);
+  pinMode(Switch, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
   attachInterrupt(digitalPinToInterrupt(interruptPin), HandleClock, RISING);
   attachInterrupt(digitalPinToInterrupt(switchinterruptPin), Run, RISING); // Note only one interrupt can be attached to an input
-  Serial.begin(9600);  
+  Serial.begin(9600);
   Serial7Segment.begin(9600); //Talk to the Serial7Segment at 9600 bps
   Serial7Segment.write('v'); //Reset the display - this forces the cursor to return to the beginning of the display
-  
+
 }
 
-void Run(){
+void Run() {
   DivPulseCounter = 0;
-  CurrentFactor=0;
-  CurrentBeatArray=0;
+  NumPulseCounter = 0;
   running = true;
   Serial7Segment.print("RRRR"); //Send serial string out the soft serial port to the S7S
-  
+
 }
 
-void Stop(){
-  running =false;
-   Serial7Segment.print("5555"); //Send serial string out the soft serial port to the S7S
+void Stop() {
+  running = false;
+  Serial7Segment.print("5555"); //Send serial string out the soft serial port to the S7S
 }
 
 void SetPin(int pin, boolean state) {
   digitalWrite(pins[pin], state);
-  
+
 }
 
 void HandleClock() {
-  if (digitalRead(Switch) ==LOW){
-     Stop();
+  if (digitalRead(Switch) == LOW) {
+    Stop();
   }
-  if (running==true){
-    
-    int tmp=DivPulseCounter;
+  if (running == true) {
+
+    int tmp = DivPulseCounter + 100 * NumPulseCounter;
     sprintf(tempString, "%4d", tmp); //Convert deciSecond into a string that is right adjusted
-  
+
     Serial7Segment.print(tempString); //Send serial string out the soft serial port to the S7S
-    if (DivPulseCounter < CurrentDivFactor){
-   
-      
+    if (DivPulseCounter < CurrentDivFactor) {
+
+
       DivPulseCounter++;
-      if (DivPulseCounter >=CurrentDivFactor){\
-         Pulse();
-         DivPulseCounter=0;
+      if (DivPulseCounter >= CurrentDivFactor) {
+        Pulse();
+        DivPulseCounter = 0;
       }
     }
-      
+    if (NumPulseCounter < CurrentPulseLength) {
+      NumPulseCounter++;
+      if (NumPulseCounter >= CurrentPulseLength) {
+        NumPulseCounter = 0;
+        CurrentPtr++;
+        if (CurrentPtr < 4) {
+          CurrentDivFactor = DivFactors[CurrentPtr];
+          CurrentPulseLength = NumberPulses[CurrentPtr];
+        } else {
+          CurrentPtr = 0;
+          CurrentDivFactor = DivFactors[CurrentPtr];
+          CurrentPulseLength = NumberPulses[CurrentPtr];
+        }
+
+      }
+    }
   }
 }
 
 
 void Pulse() {
-  
-  
-  int i=0;
+
+
+  int i = 0;
   SetPin(0, HIGH);
-  for (i=0;i<1000;i++){
-    int j=i*10; //Add some delay work
+  for (i = 0; i < 1000; i++) {
+    int j = i * 10; //Add some delay work
   }
 
   SetPin(0, LOW);
