6ea3006fe92a922f5429f1905877f9bd89fabbf1
Andy <aecobley@dundee.ac.uk>
2017-10-31 09:26:10 +0000

    updated to I2C

--- MineralFire.ino
+++ MineralFire.ino
@@ -1,5 +1,9 @@
 
 #include <Wire.h>
+#define DivMode 3
+#define ProgMode 1
+#define TempoMode 2
+#define LiveMode 3
 
 volatile byte DivPulseCounter = 0;
 volatile byte NumPulseCounter = 0;
@@ -20,7 +24,7 @@ int NumberPulses[3][8] = {{8, 8, 4, 8,8, 8, 4, 8},
 
 int DataLength=8;
 
-int Divider1[]={1,2,3}; //Adjusted for the logic of the switches
+int Divider1[]={4,8,16}; //Adjusted for the logic of the switches
 int Divider2[]={1,2,4};
 int Divider=1;
 
@@ -30,6 +34,7 @@ int Div2=1;
 byte DivSwitch = 0; //Used to set the pattern by the switches
 byte PulseSwitch = 0;
 byte ModeSwitch=1;
+byte ProgSwitch = LiveMode;
 
 volatile byte CurrentPtr = 0; //how far through the current Bars we are
 
@@ -47,9 +52,12 @@ const int SW21 = 7;
 const int SW22 = 6;
 const int SW31= A0;
 const int SW32 = A1; 
+const int PROG1 = 6;
+const int PROG2 = 7;
 
 long IntMillis =0;
-
+byte Disp[4] = {0, 0, 0, 0};
+byte OldDisp[4] = {15, 15, 15, 15};
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin 13 as an output.
@@ -62,7 +70,8 @@ void setup() {
   pinMode(SW22, INPUT);
   pinMode(SW31, INPUT);
   pinMode(SW32, INPUT);
-  
+   pinMode(PROG1, INPUT);
+  pinMode(PROG2, INPUT);
   
   attachInterrupt(digitalPinToInterrupt(interruptPin), HandleClock, FALLING);
   attachInterrupt(digitalPinToInterrupt(switchinterruptPin), Run, FALLING); // Note only one interrupt can be attached to an input
@@ -104,13 +113,10 @@ void Run() {
 
 
 void DisplayNumbers(byte A, byte B, byte C, byte  D) {
-//Send to IC2
-Wire.beginTransmission(8);
-   Wire.write(A); 
-   Wire.write(B);
-   Wire.write(C);
-   Wire.write(D);
-   Wire.endTransmission();
+  Disp[0] = A;
+  Disp[1] = B;
+  Disp[2] = C;
+  Disp[3] = D;
 }
 
 
@@ -121,6 +127,8 @@ void ReadPatternSwitches() {
   byte cSW22 = 1;
   byte cSW31 = 1;
   byte cSW32 =1;
+  byte cPROG1 = 1;
+  byte cPROG2 = 1;
   if (digitalRead(SW11) == LOW) {
     cSW11 = 0;
   }
@@ -141,17 +149,27 @@ void ReadPatternSwitches() {
   if (digitalRead(SW32) == LOW) {
     cSW32 = 0;
   }
+  if (digitalRead(PROG1) == LOW) {
+    cPROG1 = 0;
+  }
+  if (digitalRead(PROG2) == LOW) {
+    cPROG2 = 0;
+  }
   byte SW1 = 2 * cSW12 + cSW11;
   SW1 = SwitchConvert[SW1 - 1];
   byte SW2 = 2 * cSW21 + cSW22;
   SW2 = SwitchConvert[SW2 - 1];
   byte SW3 =  2 * cSW32 + cSW31;
   SW3 = SwitchConvert[SW3 - 1];
+   byte PROG =  2 * cPROG1 + cPROG2;
+  PROG = SwitchConvert[PROG - 1];
+  
   DivSwitch = SW1 - 1;
   PulseSwitch = SW2 - 1;
   ModeSwitch=SW3;
+ProgSwitch = PROG;
 
-  if (ModeSwitch==3){
+  if (ModeSwitch==DivMode){
      Div1=Divider1[SW1-1];
      Div2=Divider2[SW2-1];
      Divider=Div1*Div2;
@@ -159,12 +177,13 @@ void ReadPatternSwitches() {
 
   if (running == false) {
     if (ModeSwitch==3){
-       DisplayNumbers(SW3, Div1,Div2,Divider);
+      DisplayNumbers(63, 63,(byte)Divider/10,(byte) Divider %10);
+       //DisplayNumbers(SW3, Div1,Div2,Divider);
        //DisplayNumbers(SW3, SW1, 110, SW2);
     
     }
     else{
-      DisplayNumbers(SW3, SW1, 110, SW2);
+      DisplayNumbers(SW3, 63, SW1, SW2);
     }
   }
 }
@@ -172,10 +191,17 @@ void ReadPatternSwitches() {
 float tempo;
 void DoSimpleDivision(){
 
-    
+ 
       DivPulseCounter++;
-       DisplayTempo(tempo);
-      //DisplayNumbers(DivPulseCounter, Div1, Div2, Divider);
+      if (ProgSwitch == TempoMode) {
+    DisplayTempo(tempo);
+  } else {
+     DisplayNumbers((byte)DivPulseCounter/10,(byte) DivPulseCounter %10,(byte)Divider/10,(byte) Divider %10);
+
+//    DisplayNumbers(DivPulseCounter, Div1, Div2, Divider);
+  }
+    
+       
       if (DivPulseCounter >= Divider) {
         Pulse();
         DivPulseCounter = 0;
@@ -183,23 +209,35 @@ void DoSimpleDivision(){
    
 }
 
-void DisplayTempo(float Tempo){
-  int iTempo=(int)(Tempo);
-  //Send to IC2
- }
+void DisplayTempo(float Tempo) {
+  byte ss[4] = {0, 0, 0, 0};
+  int i = 0;
+  int iTempo = (int)(Tempo);
+  while (iTempo > 0) {
+    ss[i] = iTempo % 10;
+    iTempo = (int)(iTempo / 10);
+    i++;
+  }
+  DisplayNumbers(ss[3], ss[2], ss[1], ss[0]);
+
+
+}
 
 
 void HandleClock() {
-   ReadPatternSwitches();
+
  digitalWrite(InputLED, HIGH);  
+ sei();
   if (running == true) {
-    if (ModeSwitch==3){
+    if (ModeSwitch==DivMode){
       long NewTime=millis();
       long tTime =NewTime - IntMillis;
       IntMillis=NewTime;
       tempo = 30.0*((float)1000/(float)tTime);
      
       DoSimpleDivision();
+      digitalWrite(InputLED, LOW);
+      cli();
       return;
     }
     DisplayNumbers(CurrentPtr, CurrentPulseLength, CurrentDivFactor, DivPulseCounter);
@@ -233,7 +271,9 @@ void HandleClock() {
       }
     }
   }
-  digitalWrite(InputLED, LOW);  
+  wDelay();
+  digitalWrite(InputLED, LOW);
+  cli();
 }
 
 volatile long int work;
@@ -253,7 +293,39 @@ int k=0;
  digitalWrite(pin, LOW);  
 
 }
+
+void wDelay() {
+
+  long int i = 0;
+  int k = 0;
+  for (k = 0; k < 10; k++) {
+    for (i = 0; i < 100; i++) {
+      work = i * k; //Add some delay work
+    }
+  }
+
+}
+
+//Pack for transmission, top 2 bits are segment number
+byte packDigit(byte Digit, byte Number) {
+  byte pDigit = (byte)((Digit << 6) | Number);
+  return pDigit;
+}
 // the loop function runs over and over again forever
 void loop() {
+ReadPatternSwitches();
+  for (byte i = 0; i < 4; i++) {
 
+    if (Disp[i] != OldDisp[i]) {
+      Wire.beginTransmission(8);
+      if (Disp[i] >= 0) {
+        Wire.write(packDigit(i, OldDisp[i]));
+      }
+      else {
+        Wire.write(packDigit(i, 63));
+      }
+      Wire.endTransmission();
+      OldDisp[i] = Disp[i];
+    }
+  }
 }
